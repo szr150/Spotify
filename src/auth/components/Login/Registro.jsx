88b10232a91435baae6dbbee50dac27c6a7364bb
szr150 <jaramillomate@gmail.com>
2025-05-09 20:31:22 -0500

    Registro.jsx

--- src/auth/components/Login/Registro.jsx
+++ src/auth/components/Login/Registro.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import '../../Styles/Registro.css';
+import '../../Styles/Login.css'; 
 import { useNavigate } from 'react-router-dom';
 import { getAuth, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 import appFirebase from '../../../firebaseconfig/FireBase';
 
-// Autenticaciones externas
 import { loginWithGoogle } from '../../googleAuth';
 import { loginWithFacebook } from '../../facebookAuth';
-
 
 const auth = getAuth(appFirebase);
 
@@ -18,7 +16,7 @@ const Registro = () => {
   useEffect(() => {
     const user = auth.currentUser;
     if (user) {
-      navigate('/home');
+      navigate('/');
     }
   }, [navigate]);
 
@@ -26,7 +24,7 @@ const Registro = () => {
     try {
       const user = await loginWithGoogle();
       setUser(user);
-      navigate('/home');
+      navigate('/');
     } catch (err) {
       console.error(err.message);
     }
@@ -36,7 +34,7 @@ const Registro = () => {
     try {
       const user = await loginWithFacebook();
       setUser(user);
-      navigate('/home');
+      navigate('/');
     } catch (err) {
       console.error(err.message);
       alert("No se pudo iniciar sesión con Facebook.");
@@ -57,20 +55,20 @@ const Registro = () => {
     try {
       const result = await createUserWithEmailAndPassword(auth, correo, contraseña);
       setUser(result.user);
-      navigate('/');
+      navigate('/'); // Redirige al login
     } catch (error) {
       alert("Asegúrate de que la contraseña tenga al menos 6 caracteres.");
     }
   };
 
   return (
-    <div className="registro-container">
-      <div className="registro-box">
-        <h1 className="registro-title">Regístrate en Spotify</h1>
+    <div className="login-container">
+      <div className="login-box">
+        <h1 className="login-title">Regístrate en Spotify</h1>
         <form onSubmit={handleRegister}>
           <input type="email" placeholder="Correo electrónico" name="email" required />
           <input type="password" placeholder="Contraseña" name="password" required />
-          <button type="submit" className="registro-button">Registrarse</button>
+          <button type="submit" className="login-button">Registrarse</button>
         </form>
         <div className="social-login">
           <button className="social-button google-button" onClick={handleGoogleLogin}>
@@ -86,8 +84,8 @@ const Registro = () => {
             Continuar con Spotify
           </button>
         </div>
-        <div className="registro-footer">
-          <p>¿Ya tienes una cuenta? <a href="/login">Inicia sesión</a></p>
+        <div className="login-footer">
+          <p>¿Ya tienes una cuenta? <a href="/">Inicia sesión</a></p>
         </div>
       </div>
     </div>
